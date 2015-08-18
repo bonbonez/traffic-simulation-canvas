1bc9a95d3830d9444c983eb954680a67be3019f0
Artem <bonbonez@gmail.com>
2015-08-18 03:26:37 +0300

    Update particle.js

--- scripts/app/traffic/particle.js
+++ scripts/app/traffic/particle.js
@@ -24,11 +24,18 @@
       function TrafficParticle(config) {
         this.x = 0;
         this.y = 0;
+      
+        // radius
         this.r = Math.max(consts.TRAFFIC_PARTICLE_SIZE.min, Math.round(Math.random() * consts.TRAFFIC_PARTICLE_SIZE.max));
+        // color
         this.c = colors[Math.max(0, Math.min(Math.round(Math.random() * colors.length), colors.length - 1))];
+        // speed
         this.s = Math.max(1, Math.random() * 4);
+        // progress (from 0 to 1)
         this.p = 0;
+        // distance that particle have to move before it become destroyed
         this.d = 0;
+        // out of distance bounds flag
         this.o = false;
 
         if (config) {
